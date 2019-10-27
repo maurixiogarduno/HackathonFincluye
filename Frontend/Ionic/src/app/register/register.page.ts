5194bb219adc910cc36ac776a8a78ebb97228720
Mauricio Garduño <mau.gar.gonz@icloud.com>
2019-10-27 12:06:24 -0600

    funtional

--- Frontend/Ionic/src/app/register/register.page.ts
+++ Frontend/Ionic/src/app/register/register.page.ts
@@ -16,12 +16,12 @@ export class RegisterPage implements OnInit {
 
   validation_messages = {
    'email': [
-     { type: 'required', message: 'Email is required.' },
-     { type: 'pattern', message: 'Enter a valid email.' }
+     { type: 'required', message: 'El email es requerido.' },
+     { type: 'pattern', message: 'Ingresa un email valido.' }
    ],
    'password': [
-     { type: 'required', message: 'Password is required.' },
-     { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+     { type: 'required', message: 'La contraseña es requerida.' },
+     { type: 'minlength', message: 'La contraseña debe tener al menos 5 caracteres.' }
    ]
   };
 
@@ -49,7 +49,7 @@ export class RegisterPage implements OnInit {
      .then(res => {
        console.log(res);
        this.errorMessage = "";
-       this.successMessage = "Your account has been created. Please log in.";
+       this.successMessage = "Tu cuenta ha sido creada. Por favor Iniciar sesión.";
      }, err => {
        console.log(err);
        this.errorMessage = err.message;
